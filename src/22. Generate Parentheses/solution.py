b70d0684ac222d73fe3ceb9c12b878ebf7cb3621
Naval Kishore Barthwal <naval.kishorebarthwal@freshworks.com>
2025-06-12 07:07:33 +0530

    22. Generate Parentheses

---
+++ src/22. Generate Parentheses/solution.py
@@ -0,0 +1,23 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        res = []
+        stack = []
+        def backtrack(openCount, closedCount):
+            if openCount == closedCount == n:
+                res.append("".join(stack))
+                return
+            
+            if openCount < n:
+                stack.append("(")
+                backtrack(openCount + 1, closedCount)
+                stack.pop()
+            
+            if closedCount < openCount:
+                stack.append(")")
+                backtrack(openCount, closedCount + 1)
+                stack.pop()
+        
+
+        backtrack(0,0)
+        return res
