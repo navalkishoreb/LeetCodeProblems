bf7f5bbd51fa2404571a485e157a4f647c89b5d7
Naval Kishore Barthwal <naval.kishorebarthwal@freshworks.com>
2025-06-12 06:53:36 +0530

    10. Regular Expression Matching

--- src/RegularExpressionMatching/solution.py
+++ src/RegularExpressionMatching/solution.py
@@ -1,30 +1,33 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-
-        pattern_index = 0
-        index = 0
-        while index < len(s) and pattern_index < len(p):
-            char = s[index]
-            pattern_char = p[pattern_index]
-            if pattern_char == ".":
-                pattern_index = pattern_index + 1
-                index = index + 1
-            elif pattern_char == "*":
-                matching_char = "." if pattern_index == 0 else p[pattern_index - 1]
-                exit_loop = False
-                while index < len(s) and not exit_loop:
-                    if matching_char == ".":
-                        index = index + 1
-                    elif matching_char == char:
-                        index = index + 1
-                    else:
-                        pattern_index = pattern_index + 1
-                        index = index + 1
-                        exit_loop = True
-            elif pattern_char != char:
-                return False
-            else:
-                pattern_index = pattern_index + 1
-                index = index + 1
         
-        return index == len(s)
+        cache = {}
+        def match(string_index, pattern_index):
+
+            # print(f"{string_index} {pattern_index}")
+
+            if (string_index, pattern_index) in cache:
+                # print(f"Found in cache {string_index} {pattern_index}")
+                return  cache[(string_index,pattern_index)]
+
+            if pattern_index >= len(p) and string_index >= len(s):
+                return True
+            
+            if pattern_index >= len(p):
+                return False
+
+
+            result = False
+
+            first_match = string_index < len(s) and (p[pattern_index] == '.' or s[string_index] == p[pattern_index])
+
+            if pattern_index + 1 < len(p) and p[pattern_index + 1] == "*":
+                result =  match(string_index , pattern_index + 2) or (first_match and match(string_index + 1 , pattern_index))
+            elif first_match:
+                result = match(string_index+1 , pattern_index+1)
+
+            cache[(string_index,pattern_index )] = result
+            return result
+            
+        return match(0,0)
+
